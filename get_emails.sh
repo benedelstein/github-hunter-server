940c63848c6bb70608dcdbc9567747d301597a25
Ben Edelstein <bedelstein12@gmail.com>
2021-06-14 23:07:16 -0400

    allow user to pass in specific repo

--- get_emails.sh
+++ get_emails.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # 1 = repo url
-# 2 = temp directory
+# 2 = temp directory (usually /tmp)
 git clone "$1" "$2/emails"
 cd "$2/emails"
 git log --format="%aE" > temp.txt
-sort -u temp.txt > emails.txt
+sort -u temp.txt > emails.txt # only unique rows
 cat emails.txt
 rm -rf "$2/emails"
--- index.js
+++ index.js
@@ -1,39 +1,59 @@
 const os = require('os');
 const fetch = require('node-fetch');
-const exec = require('child_process').exec;  
+const exec = require('child_process').exec;
 
 exports.getEmails = async (request, response) => {
-    console.log("a");
+    // only allow cors requests from my extension
+    response.set('Access-Control-Allow-Origin', 'chrome-extension://pfmbgkodcdhaiendaodfomjalihnelnj');
+
+    if (request.method === 'OPTIONS') {
+        // Send response to OPTIONS requests
+        response.set('Access-Control-Allow-Methods', 'GET');
+        response.set('Access-Control-Allow-Headers', 'Content-Type');
+        response.set('Access-Control-Max-Age', '3600');
+        return response.status(204).send('');
+    }
+
     console.log(os.tmpdir());
     const username = request.query.username;
+    const repo = request.query.repo;
     if (!username) {
         response.send("invalid username");
         return;
     }
     console.log("username:", username);
+    console.log("repo name:", repo);
     // get user's github
     // get one of their repo urls (github api???)
     try {
-        const repos = await fetch(`https://api.github.com/users/${username}/repos`);
-        const json = await repos.json();
-        console.log(json[0]);
-        const url = json[0]['html_url'];
-        console.log("repo url", url);
-        let urls = [];
-        for (let i = 0; i<json.length; i++) {
-            urls.push(json[i]['html_url']);
+        let url;
+        if (repo != "") { // if user called script from a specific repo page, get the emails for that repo
+            url = `https://github.com/${username}/${repo}`;
+        } else {
+            // fetch the user's repos from api
+            const repos = await fetch(`https://api.github.com/users/${username}/repos`);
+            const json = await repos.json();
+            url = json[0]['html_url'];
+            console.log("repo url", url);
+            let urls = [];
+            for (let i = 0; i<json.length; i++) {
+                urls.push(json[i]['html_url']);
+            }
+            console.log("all urls", urls);
         }
-        console.log("all urls", urls);
         // pass that url into the shell script
-        
         // todo: maybe use async version instead, maybe run multiple in parallel with diff urls
         exec(`./get_emails.sh ${url} ${os.tmpdir()}`, (err, stdout, stderr) => {  
             if (err) {  
-                console.error(err);  
+                console.error(err);
+                response.status(500).send(error);  
                 return;  
             }  
             console.log(stdout);
-            let emails = stdout.split("\n").filter(e => !e.endsWith("@users.noreply.github.com") && e != "");
+            let emails = stdout.split("\n").filter(e => {
+                console.log(e, e.endsWith("@users.noreply.github.com"), e != "");
+                return !e.endsWith("@users.noreply.github.com") && e != "";
+            });
             console.log(emails);
 
             // send the outputted emails back to the client
